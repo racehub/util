330d694ea4710bfc740b7f304eb8091cb660ad9b
Sam Ritchie <sritchie09@gmail.com>
2014-06-04 10:52:10 -0400

    saving.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject paddleguru/util "0.1.0"
+(defproject paddleguru/util "0.1.1"
   :description "Utility functions from PaddleGuru."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"

301aed32f5117d7a92a4698ea96b75e4a702da82
David Petrovics <dpetrovics@gmail.com>
2014-07-27 19:37:07 -0400

    lein-release plugin: bumped version from 0.1.5 to 0.1.6-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject racehub/util "0.1.5"
+(defproject racehub/util "0.1.6-SNAPSHOT"
   :description "Utility functions from RaceHub."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"

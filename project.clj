3e761939c53fe0718d8afc52450c00a4dbab43b8
David Petrovics <dpetrovics@gmail.com>
2014-11-15 18:44:16 -0800

    lein-release plugin: preparing 0.2.9 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject racehub/util "0.2.9-SNAPSHOT"
+(defproject racehub/util "0.2.9"
   :description "Utility functions from RaceHub."
   :url "https://github.com.com/racehub/util"
   :license {:name "Eclipse Public License"

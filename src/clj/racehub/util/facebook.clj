9f5f14ccb5cae21b129eecd264355865d456afae
Sam Ritchie <sritchie09@gmail.com>
2014-12-06 12:35:24 -0700

    more facebook features

--- src/clj/racehub/util/facebook.clj
+++ src/clj/racehub/util/facebook.clj
@@ -1,6 +1,7 @@
 (ns racehub.util.facebook
   "Facebook API helpers."
-  (:require [cheshire.core :refer [parse-string]]
+  (:require [clojure.string :as str]
+            [cheshire.core :refer [parse-string generate-string]]
             [org.httpkit.client :as http]
             [racehub.util.config :as c]
             [racehub.util.oauth :as oauth]
@@ -11,6 +12,10 @@
 ;; FB_CLIENT_SECRET environment variables.
 
 ;; ## Schema
+
+(s/defschema APIError
+  "Error response from the Facebook API."
+  {:error {:message s/Str :type s/Str :code s/Int}})
 
 (s/defschema Paging
   "Not really sure how this is used."
@@ -23,7 +28,7 @@
 
 ;; ## Security
 
-(def api-version "2.1")
+(def api-version "2.2")
 (def root (str "https://graph.facebook.com/v" api-version "/"))
 
 (s/defn facebook-config :- FacebookConfig
@@ -66,18 +71,115 @@
      @(http/get (str root "oauth/access_token")
                 {:query-params params}))))
 
-;; ## Api Calls
-
-(s/defn me :- {s/Any s/Any}
+(s/defn api-get :- {s/Any s/Any}
   "Makes a call to Facebook's /user/id endpoint for the user linked to
   the supplied token. Gives you a map with a bunch of information
   about the user.
 
   See the documentation for more details on what the map holds:
   https://developers.facebook.com/docs/graph-api/reference/v2.1/user"
+  [endpoint :- s/Str
+   {:keys [query-params params]}
+   :- {:query-params {:token s/Str, s/Any s/Any}
+       :params {s/Any s/Any}}]
+  (let [opts (assoc params :query-params query-params)]
+    (body (http/get (str root endpoint) opts))))
+
+;; ## Api Calls
+
+;; ### Token Exchange
+;;
+;; This section deals with exchanging short-lived client tokens for
+;;long-lived server tokens.
+
+(s/defn exchange-token :- (s/either APIError {:token s/Str})
   [token :- s/Str]
-  (let [opts {:query-params {"access_token" token}}]
-    (body (http/get (str root "me") opts))))
+  (let [{:keys [client-id client-secret]} (:oauth (facebook-config))
+        resp @(http/get (str root "oauth/access_token")
+                        {:query-params
+                         {:grant_type "fb_exchange_token"
+                          :fb_exchange_token token
+                          :client_id client-id
+                          :client_secret client-secret}})]
+    (or (when-let [token (oauth/get-access-token-from-params resp)]
+          {:token token})
+        (parse-string (:body resp) keyword))))
+
+;; ### Profile Info
+
+(s/defschema UserData
+  "Response from Facebook's me endpoint."
+  {:email s/Str
+   :first_name s/Str
+   :timezone (s/named s/Int "UTC offset.")
+   :locale s/Str
+   :name s/Str
+   :updated_time s/Str
+   :link s/Str
+   :id s/Str
+   :last_name s/Str
+   :gender s/Str
+   :verified s/Bool
+   s/Any s/Any})
+
+(s/defn me :- UserData
+  "Makes a call to Facebook's /user/id endpoint for the user linked to
+  the supplied token. Gives you a map with a bunch of information
+  about the user.
+
+  Optionall you can give a list of fields
+
+  See the documentation for more details on what the map holds:
+  https://developers.facebook.com/docs/graph-api/reference/v2.1/user"
+  ([token :- s/Str]
+     (me token nil))
+  ([token :- s/Str fields :- [s/Str]]
+     (api-get "me" {:query-params {:access_token token
+                                   :fields (str/join "," fields)}})))
+
+(s/defschema CoverPhoto
+  {:id s/Str
+   (s/optional-key :offset_y) s/Int
+   (s/optional-key :source) s/Str})
+
+(s/defschema ProfilePhoto
+  {:is_silhouette s/Bool
+   :url s/Str
+   (s/optional-key :width) s/Int
+   (s/optional-key :height) s/Int})
+
+(s/defn cover-photo :- CoverPhoto
+  "Returns information for the supplied user's cover photo. If the
+  user's cover photo isn't set, only the :id is present."
+  [token :- s/Str]
+  (:cover (me token ["cover"])))
+
+(s/defn profile-photo :- (s/maybe ProfilePhoto)
+  "If the supplied user exists, returns information for the supplied
+  user's cover photo. If the user's cover photo isn't set, only
+  the :id is present.
+
+  The actual API can take width and height parameters. These aren't
+  implemented yet."
+  [facebook-id :- s/Str]
+  (:data
+   (api-get (str facebook-id "/picture")
+            {:query-params {:type "large" :redirect false}
+             :params {:oauth-token (server-secret)}})))
+
+(comment
+  "Example of a batch request."
+  (s/defn photos
+    [facebook-id]
+    (body
+     (http/post "https://graph.facebook.com"
+                {:query-params
+                 {:batch (generate-string
+                          [{:method "GET" :relative_url (str facebook-id "?fields=cover")}
+                           {:method "GET" :relative_url (str facebook-id "/picture?type=large&redirect=false")}])}
+                 :oauth-token (server-secret)}))))
+
+;; ## Test Users
 
 (s/defschema TestUser
   "Fields for the test user endpoint."
@@ -89,6 +191,5 @@
   "Queries the /accounts/test-users endpoint:
   https://developers.facebook.com/docs/graph-api/reference/v2.1/app/accounts/test-users"
   []
-  (body
-   (http/get (str root (app-id) "/accounts/test-users")
-             {:oauth-token (server-secret)})))
+  (api-get (str (app-id) "/accounts/test-users")
+           {:params {:oauth-token (server-secret)}}))
